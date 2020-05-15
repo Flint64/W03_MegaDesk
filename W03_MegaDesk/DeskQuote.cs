7208faf216226b43a5f67c72c7bcd2d3f8c9aa6d
Flint64 <twda96@gmail.com>
2020-05-15 16:05:26 -0600

    Added reading from file, changed hard-coded in rush prices to the ones from the newly created array

--- W03_MegaDesk/DeskQuote.cs
+++ W03_MegaDesk/DeskQuote.cs
@@ -13,7 +13,10 @@ namespace W03_MegaDesk {
         public string customerName;
         public Desk desk;
 
+        decimal[,] rushPrices = new decimal[3, 3];
+
         public decimal getQuote() {
+
 
             //Desk base price
             decimal price = 200;
@@ -24,7 +27,16 @@ namespace W03_MegaDesk {
             }
 
             //$50 for each drawer
-            price += (desk.NumberOfDrawers * 50);
+            switch (desk.NumberOfDrawers) {
+                case (NumDrawers.Zero) : price += 0; break;
+                case (NumDrawers.One)  : price += 50; break;
+                case (NumDrawers.Two)  : price += (50 * 2); break;
+                case (NumDrawers.Three): price += (50 * 3); break;
+                case (NumDrawers.Four) : price += (50 * 4); break;
+                case (NumDrawers.Five) : price += (50 * 5); break;
+                case (NumDrawers.Six)  : price += (50 * 6); break;
+                case (NumDrawers.Seven): price += (50 * 7); break;
+            }
 
             //Check the surface material choice and 
             //add the correct amount to the price
@@ -36,12 +48,43 @@ namespace W03_MegaDesk {
                 case SurfaceMaterial.Veneer:   { price += 125; break; }
             }
 
-
             //Check rush order options and add to price
             price += calcRushPrice(desk.RushOption);
 
 
             return price;
+        }
+
+        private void loadRushPrices() {
+            int counter = 0;
+            int position = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(@"rushOrderPrices.txt");
+            while ((line = file.ReadLine()) != null) {
+
+                if (counter < 3) {
+                    rushPrices[0, position] = Decimal.Parse(line);
+                }
+                if (counter > 2 && counter < 6) {
+                    rushPrices[1, position] = Decimal.Parse(line);
+                }
+                if (counter > 5 && counter < 9) {
+                    rushPrices[2, position] = Decimal.Parse(line);
+                }
+
+                counter++;
+                position++;
+
+                if (position > 2) {
+                    position = 0;
+                }
+
+            }
+
+            file.Close();
+
         }
 
 
@@ -50,7 +93,8 @@ namespace W03_MegaDesk {
          * and surface area. Returns rush price.
          *********************************************************/
         private decimal calcRushPrice(RushOption option) {
-            
+
+            loadRushPrices();
             decimal rushPrice = 0;
 
             switch (option) {
@@ -61,22 +105,22 @@ namespace W03_MegaDesk {
 
                 case RushOption.day_3: {
                     if (desk.SurfaceArea < 1000) {
-                        rushPrice = 60;
+                        rushPrice = rushPrices[0,0];
                     } else if (desk.SurfaceArea >= 1000 && desk.SurfaceArea <= 2000) {
-                        rushPrice = 70;
+                        rushPrice = rushPrices[0, 1];
                     } else if (desk.SurfaceArea > 2000) {
-                        rushPrice = 80;
+                        rushPrice = rushPrices[0, 2];
                     }
                     break;
                 }
 
                 case RushOption.day_5: {
                     if (desk.SurfaceArea < 1000) {
-                        rushPrice = 40;
+                        rushPrice = rushPrices[1, 0];
                     } else if (desk.SurfaceArea >= 1000 && desk.SurfaceArea <= 2000) {
-                        rushPrice = 50;
+                        rushPrice = rushPrices[1, 1];
                     } else if (desk.SurfaceArea > 2000) {
-                        rushPrice = 60;
+                        rushPrice = rushPrices[1, 2];
                     }
                     break;
                 }
