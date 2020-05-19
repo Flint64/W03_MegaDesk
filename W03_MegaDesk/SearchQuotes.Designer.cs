93267b5359eefd2ee01cd7536c74c61735e5c116
dwilkin7 <wil17012@byui.edu>
2020-05-19 15:39:05 -0600

    Added comboBox, label, datagrid to SearchQuotes. Created quotes list to appear in datagrid based on comboBox materials selection.

--- W03_MegaDesk/SearchQuotes.Designer.cs
+++ W03_MegaDesk/SearchQuotes.Designer.cs
@@ -23,20 +23,62 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBoxMaterials = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 171);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1043, 371);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBoxMaterials
+            // 
+            this.comboBoxMaterials.FormattingEnabled = true;
+            this.comboBoxMaterials.Location = new System.Drawing.Point(426, 59);
+            this.comboBoxMaterials.Name = "comboBoxMaterials";
+            this.comboBoxMaterials.Size = new System.Drawing.Size(201, 24);
+            this.comboBoxMaterials.TabIndex = 1;
+            this.comboBoxMaterials.SelectedIndexChanged += new System.EventHandler(this.comboBoxMaterials_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Surface Material:";
             // 
             // SearchQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxMaterials);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SearchQuotes";
             this.Text = "SearchQuotes";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SearchQuotes_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBoxMaterials;
+        private System.Windows.Forms.Label label1;
     }
 }
--- W03_MegaDesk/SearchQuotes.cs
+++ W03_MegaDesk/SearchQuotes.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace W03_MegaDesk {
     public partial class SearchQuotes : Form {
@@ -18,10 +20,78 @@ namespace W03_MegaDesk {
 
             _mainMenu = mainMenu;
 
+            //This will populate the combobox with the surface materials list
+            List<SurfaceMaterial> materials = Enum.GetValues(typeof(SurfaceMaterial))
+                .Cast<SurfaceMaterial>()
+                .ToList();
+            comboBoxMaterials.DataSource = materials;
+            comboBoxMaterials.SelectedIndex = -1;
+
         }
 
+        private void loadGrid()
+        {
+            var quotesFile = @"quotes.json";
+
+            using (StreamReader reader = new StreamReader(quotesFile))
+            {
+                //This loads in the existing quotes
+                string quotes = reader.ReadToEnd();
+                List<DeskQuote> deskQuotes = JsonConvert.DeserializeObject<List<DeskQuote>>(quotes);
+
+                dataGridView1.DataSource = deskQuotes.Select(d => new
+                {
+                    Date = d.currentDate,
+                    Customer = d.customerName,
+                    Depth = d.desk.Depth,
+                    Width = d.desk.Width,
+                    Drawers = d.desk.NumberOfDrawers,
+                    SurfaceMaterial = d.desk.SurfaceMaterial,
+                    DeliveryType = d.desk.RushOption,
+                    QuoteAmount = d.price.ToString("c")
+                }).ToList();
+            }
+        }
+
+        private void loadGrid(SurfaceMaterial desktopMaterial)
+        {
+            var quotesFile = @"quotes.json";
+
+            using (StreamReader reader = new StreamReader(quotesFile))
+            {
+                //This loads in the existing quotes
+                string quotes = reader.ReadToEnd();
+                List<DeskQuote> deskQuotes = JsonConvert.DeserializeObject<List<DeskQuote>>(quotes);
+
+                dataGridView1.DataSource = deskQuotes.Select(d => new
+                {
+                    Date = d.currentDate,
+                    Customer = d.customerName,
+                    Depth = d.desk.Depth,
+                    Width = d.desk.Width,
+                    Drawers = d.desk.NumberOfDrawers,
+                    SurfaceMaterial = d.desk.SurfaceMaterial,
+                    DeliveryType = d.desk.RushOption,
+                    QuoteAmount = d.price.ToString("c")
+                }).Where(q => q.SurfaceMaterial == desktopMaterial)
+                .ToList();
+            }
+        }
         private void SearchQuotes_FormClosed(object sender, FormClosedEventArgs e) {
             _mainMenu.Show();
+        }
+
+        private void comboBoxMaterials_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox combo = (ComboBox)sender;
+            if(combo.SelectedIndex < 0)
+            {
+                loadGrid();
+            }
+            else
+            {
+                loadGrid((SurfaceMaterial)combo.SelectedValue);
+            }
         }
     }
 }
